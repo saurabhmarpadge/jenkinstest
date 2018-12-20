c72b568bd44b2b1fb0103b442ab263616ed234db
Saurabhkumar Marpadge <saurabhmarpadge29@gmail.com>
2018-12-20 14:15:44 +0530

    Add files via upload

---
+++ src/main/java/com/jenkins/App.java
@@ -0,0 +1,13 @@
+package com.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String getString(){
+        return "String";
+    }
+
+}
---
+++ src/test/java/com/jenkins/AppTest.java
@@ -0,0 +1,22 @@
+package com.jenkins;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    App app = new App();
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals("String",app.getString());
+    }
+}
